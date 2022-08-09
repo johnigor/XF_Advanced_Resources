ef4b745e8aa91522d0b309ef7f4af19237e82db5
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-08-09 16:44:47 -0300

    Creating simple groups

--- ListView/App02/App02/MainPage.xaml.cs
+++ ListView/App02/App02/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace App02
@@ -13,6 +8,46 @@ namespace App02
         public MainPage()
         {
             InitializeComponent();
+
+            ListaFuncionarios.ItemsSource = GetFuncionarios();
+        }
+
+        private List<Grupo> GetFuncionarios()
+        {
+            return new List<Grupo>
+            {
+                new Grupo ("Presidente", "CEO")
+                {
+                    new Pessoa { Nome = "José" },
+                },
+                new Grupo ("Diretores", "Dir.")
+                {
+                    new Pessoa { Nome = "João" },
+                    new Pessoa { Nome = "Maria" },
+                },
+                new Grupo ("Gerentes", "Ger.")
+                {
+                    new Pessoa { Nome = "Felipe" },
+                    new Pessoa { Nome = "Judas" },
+                }
+            };
+        }
+
+        public class Grupo : List<Pessoa>
+        {
+            public string Titulo { get; set; }
+            public string Abreviacao { get; set; }
+
+            public Grupo(string titulo, string abreviacao)
+            {
+                Titulo = titulo;
+                Abreviacao = abreviacao;
+            }
+        }
+
+        public class Pessoa
+        {
+            public string Nome { get; set; }
         }
     }
 }
--- ListView/App02/App02/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ ListView/App02/App02/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace App02 {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ListView ListaFuncionarios;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            ListaFuncionarios = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "ListaFuncionarios");
         }
     }
 }
