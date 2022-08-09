cddd2a44472f725589c7a3f6d70813788feabfbe
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-08-09 14:41:02 -0300

    Behavior example

---
+++ Behavior_Triggers/App01/App01/Lib/MaiusculoBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace App01.Lib
+{
+    public class MaiusculoBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += ConverterParaMaiusculo;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= ConverterParaMaiusculo;
+        }
+
+        private void ConverterParaMaiusculo(object sender, TextChangedEventArgs args)
+        {
+            if (!string.IsNullOrEmpty(args.NewTextValue))
+            {
+                ((Entry)sender).Text = args.NewTextValue.ToUpper();
+            }
+        }
+    }
+}
