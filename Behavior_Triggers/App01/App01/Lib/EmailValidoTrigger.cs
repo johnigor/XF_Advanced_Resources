9a306c60c7cfdee9ac9c4c5a965b218e50561df5
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-08-09 15:33:23 -0300

    Triggers based on events

---
+++ Behavior_Triggers/App01/App01/Lib/EmailValidoTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace App01.Lib
+{
+    public class EmailValidoTrigger : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            if (sender.Text.IndexOf("@") >= 0)
+            {
+                sender.TextColor = Color.Green;
+            }
+            else
+            {
+                sender.TextColor = Color.Red;
+            }
+        }
+    }
+}
--- Behavior_Triggers/App01/App01/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ Behavior_Triggers/App01/App01/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -20,9 +20,13 @@ namespace App01 {
         private global::Xamarin.Forms.Entry TxtNome;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry TxtEmail;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
             TxtNome = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "TxtNome");
+            TxtEmail = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "TxtEmail");
         }
     }
 }
