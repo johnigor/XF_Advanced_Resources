6de17d85c38e349c7c4939f9f5cc2a16f1b8b894
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-08-09 17:28:02 -0300

    DataTemplate Selector

--- ListView/App02/App02/MainPage.xaml.cs
+++ ListView/App02/App02/MainPage.xaml.cs
@@ -18,29 +18,29 @@ namespace App02
             {
                 new Grupo ("Presidente", "CEO", "Presidente da empresa")
                 {
-                    new Pessoa { Nome = "José" },
+                    new Pessoa { Nome = "José", IsRequired = true, Eficiencia = 8},
                 },
                 new Grupo ("Diretores", "Dir.", "Fazem parte do conselho e auxiliam nas tomadas de decisões")
                 {
-                    new Pessoa { Nome = "João" },
-                    new Pessoa { Nome = "Maria" },
+                    new Pessoa { Nome = "João", IsRequired = false },
+                    new Pessoa { Nome = "Maria", IsRequired = true, Eficiencia = 8 },
                 },
                 new Grupo ("Gerentes", "Ger.", "Gerenciam o desempenho e funções dos funcionários")
                 {
-                    new Pessoa { Nome = "Felipe" },
-                    new Pessoa { Nome = "Judas" },
+                    new Pessoa { Nome = "Felipe", IsRequired = true, Eficiencia = 9 },
+                    new Pessoa { Nome = "Judas", IsRequired = false },
                 },
                 new Grupo ("Funcionários", "Func.", "Realizam atividades específicas com base em sua profissão")
                 {
-                    new Pessoa { Nome = "Lucas" },
-                    new Pessoa { Nome = "Danilo" },
-                    new Pessoa { Nome = "Jéssica" },
-                    new Pessoa { Nome = "Pedro" },
-                    new Pessoa { Nome = "Fernanda" },
-                    new Pessoa { Nome = "Ellen" },
-                    new Pessoa { Nome = "Luan" },
-                    new Pessoa { Nome = "Carlos" },
-                    new Pessoa { Nome = "Amanda" },
+                    new Pessoa { Nome = "Lucas", IsRequired = false },
+                    new Pessoa { Nome = "Danilo", IsRequired = false },
+                    new Pessoa { Nome = "Jéssica", IsRequired = false },
+                    new Pessoa { Nome = "Pedro", IsRequired = true, Eficiencia = 6 },
+                    new Pessoa { Nome = "Fernanda", IsRequired = false },
+                    new Pessoa { Nome = "Ellen", IsRequired = false },
+                    new Pessoa { Nome = "Luan", IsRequired = false },
+                    new Pessoa { Nome = "Carlos", IsRequired = false },
+                    new Pessoa { Nome = "Amanda", IsRequired = false },
                 }
             };
         }
@@ -62,6 +62,8 @@ namespace App02
         public class Pessoa
         {
             public string Nome { get; set; }
+            public double Eficiencia { get; set; } //0 a 10
+            public bool IsRequired { get; set; }
         }
     }
 }
---
+++ ListView/App02/App02/SeletorTemplate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using static App02.MainPage;
+
+namespace App02
+{
+    public class SeletorTemplate : DataTemplateSelector
+    {
+
+        DataTemplate PessoaFundamental;
+        DataTemplate PessoaNaoFundamental;
+
+        public SeletorTemplate()
+        {
+            PessoaFundamental = new DataTemplate(typeof(Templates.PessoaFundamentalViewCell));
+            PessoaNaoFundamental = new DataTemplate (typeof(Templates.PessoaNaoFundamentalViewCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            Pessoa pessoa = item as Pessoa;
+
+            if (pessoa.IsRequired)
+            {
+                return PessoaFundamental;
+            }
+            else
+            {
+                return PessoaNaoFundamental;
+            }
+        }
+    }
+}
