6a9f74e054f252790a435b28e31f0b0cf6a889f0
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-08-09 15:12:27 -0300

    Binding a trigger into a button

--- Behavior_Triggers/App01/App01/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ Behavior_Triggers/App01/App01/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace App01 {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry TxtNome;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            TxtNome = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "TxtNome");
         }
     }
 }
