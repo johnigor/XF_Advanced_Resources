f0bb40ffc9276c5d6a85c5783cdd4a6e39b68641
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-08-10 16:04:11 -0300

    Control "TituloCor" made to change the color of the titles

--- CustomControl/App05/App05/Controls/MyControl.xaml.cs
+++ CustomControl/App05/App05/Controls/MyControl.xaml.cs
@@ -7,6 +7,19 @@ namespace App05.Controls
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MyControl : ContentView
     {
+        public string Titulo
+        {
+            get { return (string)GetValue(TituloProperty); }
+            set { SetValue(TituloProperty, value); }
+        }
+
+        public Color TituloCor
+        {
+            get { return (Color)GetValue(TituloCorProperty); }
+            set { SetValue(TituloCorProperty, value); }
+        }
+
+        //Title
         public static readonly BindableProperty TituloProperty = BindableProperty.Create(
             propertyName: "Titulo",
             returnType: typeof(string),
@@ -16,16 +29,31 @@ namespace App05.Controls
             propertyChanged: TituloPropertyChanged
             );
 
-        public string Titulo
-        {
-            get { return (string)GetValue(TituloProperty); }
-            set { SetValue(TituloProperty, value); }
-        }
-
         private static void TituloPropertyChanged(BindableObject bindable, Object oldValue, Object newValue)
         {
+            var myControl = (MyControl)bindable;
+
+            myControl.titulo.Text = (string)newValue;
 
         }
+
+        //Title Color
+        public static readonly BindableProperty TituloCorProperty = BindableProperty.Create(
+            propertyName: "TituloCor",
+            returnType: typeof(Color),
+            declaringType: typeof(MyControl),
+            defaultValue: Color.Black,
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: TituloCorPropertyChanged
+            );
+
+        private static void TituloCorPropertyChanged(BindableObject bindable, Object oldValue, Object newValue)
+        {
+            var myControl = (MyControl)bindable;
+
+            myControl.titulo.TextColor = (Color)newValue;
+        }
+
         public MyControl()
         {
             InitializeComponent();
