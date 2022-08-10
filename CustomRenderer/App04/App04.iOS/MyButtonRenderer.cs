09ee419d71e53a29a607e8ee311a26b966a29ace
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-08-10 13:19:16 -0300

    Setting native customs for Button

---
+++ CustomRenderer/App04/App04.iOS/MyButtonRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(App04.Customs.MyButton), typeof(App04.iOS.MyButtonRenderer))]
+namespace App04.iOS
+{
+    internal class MyButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.Layer.CornerRadius = 10;
+                Control.ClipsToBounds = true;
+            }
+        }
+    }
+}
