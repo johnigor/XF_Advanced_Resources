c2ad38214553925dad0ddee8a8b568fab4a01e95
John Hansen <JOHN00IGOR@GMAIL.COM>
2022-08-22 15:18:41 -0300

    Criando arquivo "CustomControlNative" para costumização nativa

---
+++ CustomControlNative/App06/App06/Controls/CustomBoxView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App06.Controls
+{
+    internal class CustomBoxView : BoxView
+    {
+        public static readonly BindableProperty espessuraProperty = BindableProperty.Create<CustomBoxView, double>(
+            p => p.Espessura, default(double)
+            );
+
+        public double Espessura 
+        {
+            get { return (double)GetValue(espessuraProperty); }
+            set { SetValue(espessuraProperty, value); }
+        }
+    }
+}
